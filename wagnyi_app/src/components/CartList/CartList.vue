4e0c1aeb6e1864ba6229df9a8bd07576e5ac8859
yuanguaangcun <1599190138@qq.com>
2020-06-17 22:04:46 +0800

    提交

--- wagnyi_app/src/components/CartList/CartList.vue
+++ wagnyi_app/src/components/CartList/CartList.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="cartListContaiter">
-    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>1</van-swipe-item>
-      <van-swipe-item>2</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" @change="onChange">
+      <van-swipe-item v-for="(item, index) in cartListData.bannerUrlList" :key="index">
+        <img class="bannerImg" v-lazy="item" alt="">
+      </van-swipe-item>
+      <div class="custom-indicator" slot="indicator">
+        <span v-for="(item, index) in cartListData.bannerUrlList" :class="{active:banIndex===index}"></span>
+      </div>
     </van-swipe>
   </div>
 </template>
@@ -18,12 +20,20 @@ import {mapState} from 'vuex';
 export default {
   props:['navId'],
   name: 'CartList',
+  data() {
+    return {
+      banIndex:0,
+    }
+  },
   computed: {
     ...mapState({
       indexDataNav:state=>state.indexData.indexDataNav,
     }),
     cartListData(){
       return this.indexDataNav.find(item=>item.id===this.navId)
+    },
+    onChange(index){
+      this.banIndex=index
     }
   },
 }
@@ -31,11 +41,26 @@ export default {
 
 <style lang="stylus" scoped>
 .cartListContaiter
-  .my-swipe .van-swipe-item {
-    color: #fff;
-    font-size: 20px;
-    line-height: 150px;
-    text-align: center;
-    background-color: #39a9ed;
-  }
+  .my-swipe .van-swipe-item 
+    color #fff
+    text-align center
+    .bannerImg
+      width 100%
+      height 370px
+  .custom-indicator
+    height 10px
+    display inline
+    position absolute
+    bottom 20px
+    left 50%  
+    transform translateX(-50%)
+    span 
+      float left
+      width 40px
+      height 4px
+      background #fff
+      opacity .5
+      margin 0 5px
+      &.active
+        opacity 1
 </style>
